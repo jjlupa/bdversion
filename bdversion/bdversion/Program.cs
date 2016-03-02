b7d36c18dea1cbfa5e0cb87da5bf320fb91e2578
jjlupa <jjlupa@gmail.com>
2016-03-01 20:35:04 -0500

    I just want to get the frame up here.

---
+++ bdversion/bdversion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Build.Client;
+
+namespace bdversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri tfsUri = new Uri("http://tfstta.int.thomson.com:8080/tfs");
+
+            
+ 
+        }
+    }
+}
